2c0bb0dda78cc642e6a91703fc1991257c477cac
AnnaAfonina <a.a.a.2711@mail.ru>
2016-05-12 11:45:44 +0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,90 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import main.Const;
+
+
+public class Client {
+    private BufferedReader in;
+    private PrintWriter out;
+    private Socket socket;
+
+
+    public Client() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("IP Adresse (Localhost 127.0.0.1).");
+        System.out.println("Format: xxx.xxx.xxx.xxx");
+
+        String ip = scan.nextLine();
+
+        try {
+
+            socket = new Socket(ip, Const.Port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println("Имя в чате");
+            out.println(scan.nextLine());
+
+
+            Resender resend = new Resender();
+            resend.start();
+
+
+            String str = "";
+            while (!str.equals("exit")) {
+                str = scan.nextLine();
+                out.println(str);
+            }
+            resend.setStop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+    }
+
+
+    private void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (Exception e) {
+            System.err.println("Error!");
+        }
+    }
+
+
+    private class Resender extends Thread {
+
+        private boolean stoped;
+
+
+        public void setStop() {
+            stoped = true;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                while (!stoped) {
+                    String str = in.readLine();
+                    System.out.println(str);
+                }
+            } catch (IOException e) {
+                System.err.println("Error");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
